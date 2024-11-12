b71806f5fc18877989eb5dd0f136b0fd5f547be7
OWEE\trlfr <tr.lfrostbyte@gmail.com>
2024-11-12 23:43:53 +0800

    Add battlepass functionality for step counter scene

--- Assets/Scripts/Jiawei UI Scripts/MyStepsEvents.cs
+++ Assets/Scripts/Jiawei UI Scripts/MyStepsEvents.cs
@@ -5,6 +5,20 @@ using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEngine.SceneManagement;
 
+public class Reward
+{
+    public int stepsNeeded;
+    public int waterReward;
+    public int fertilizerReward;
+
+    public Reward(int steps, int water, int fert)
+    {
+        stepsNeeded = steps;
+        waterReward = water;
+        fertilizerReward = fert;
+    }
+}
+
 public class MyStepsEvents : MonoBehaviour
 {
     private UIDocument document;
@@ -15,18 +29,72 @@ public class MyStepsEvents : MonoBehaviour
     private Label stepCount;
 
     private List<Button> menuButtons = new List<Button>();
+    private List<Button> goalButtons = new List<Button>();
 
     private AudioSource audioSource;
 
     [SerializeField]
-    PlayerDataSO playerDataSO;
+    PlayerDataSO playerDataSO; // Should not use to get steps
+    [SerializeField]
+    SaveManagerSO saveManagerSO;
 
     [SerializeField]
     private List<Sprite> collectedSprites;
+    [SerializeField]
     private List<Sprite> uncollectedSprites;
+
+    private List<Reward> goals = new List<Reward>();
+
+    private void InstantiateGoals() // Bad practice: All these are hardcoded as the images are hardcoded too
+    {
+        goals.Add(new Reward(steps: 8000, water: 2, fert: 2)); // 1
+        goals.Add(new Reward(steps: 18000, water: 3, fert: 0)); // 2
+        goals.Add(new Reward(steps: 24000, water: 2, fert: 2)); // 3
+        goals.Add(new Reward(steps: 32000, water: 0, fert: 3)); // 4
+        goals.Add(new Reward(steps: 40000, water: 4, fert: 4)); // 5
+        goals.Add(new Reward(steps: 48000, water: 4, fert: 4)); // 6
+        goals.Add(new Reward(steps: 56000, water: 6, fert: 0)); // 7
+        goals.Add(new Reward(steps: 64000, water: 6, fert: 6)); // 8
+        goals.Add(new Reward(steps: 72000, water: 0, fert: 6)); // 9
+        goals.Add(new Reward(steps: 80000, water: 6, fert: 6)); // 10
+        goals.Add(new Reward(steps: 88000, water: 8, fert: 8)); // 11
+        goals.Add(new Reward(steps: 96000, water: 9, fert: 0)); // 12
+        goals.Add(new Reward(steps: 104000, water: 8, fert: 8)); // 13
+        goals.Add(new Reward(steps: 112000, water: 0, fert: 9)); // 14
+        goals.Add(new Reward(steps: 124000, water: 10, fert: 10)); // 15
+    }
+
+    private void PurchaseReward(int index)
+    {
+        Reward chosenReward = goals[index];
+        // Check if player can purchase reward
+        if (!playerDataSO.GetHasCollectedRewardStatus(index))
+        {
+            if (counter.GetStepCount() >= chosenReward.stepsNeeded)
+            {
+                string popupMessage = $"You have earned {chosenReward.waterReward} bag(s) of water and {chosenReward.fertilizerReward} bag(s) of fertilizer";
+                // Popup(popupMessage);
+                // Update playerDataSO
+                playerDataSO.SetFertilizer(playerDataSO.GetFertilizer() + chosenReward.fertilizerReward);
+                playerDataSO.SetWater(playerDataSO.GetWater() + chosenReward.waterReward);
+                playerDataSO.SetHasCollectedRewardStatus(index, true);
+                saveManagerSO.Save();
+
+                makeButtonCollected(index);
+            }
+        }
+    }
+
+    private void makeButtonCollected(int index)
+    {
+        // Change the image
+        var texture = collectedSprites[index].texture;
+        goalButtons[index].style.backgroundImage = new StyleBackground(texture);
+    }
 
     private void Awake()
     {
+        InstantiateGoals();
         audioSource = GetComponent<AudioSource>();
         document = GetComponent<UIDocument>();
         counter = GetComponent<StepCounter>();
@@ -44,7 +112,30 @@ public class MyStepsEvents : MonoBehaviour
         for (int i = 0; i < menuButtons.Count; i++)
         {
             menuButtons[i].RegisterCallback<ClickEvent>(OnAllButtonsClick);
-            
+        }
+
+        // Loop through the names "Goal1" to "Goal15" and find the corresponding buttons
+        for (int i = 1; i <= 15; i++)
+        {
+            string buttonName = "Goal" + i; // Construct button name
+            Button button = document.rootVisualElement.Q<Button>(buttonName);
+
+            if (button != null)
+            {
+                int index = i - 1;
+                goalButtons.Add(button); // Add the button to the list
+                if (playerDataSO.GetHasCollectedRewardStatus(index))
+                {
+                    makeButtonCollected(index);
+                } else
+                {
+                    button.clicked += () => PurchaseReward(index); // Example: adding a click event
+                }
+            }
+            else
+            {
+                Debug.LogWarning("Button with name " + buttonName + " not found.");
+            }
         }
 
         int steps = counter.GetStepCount();
