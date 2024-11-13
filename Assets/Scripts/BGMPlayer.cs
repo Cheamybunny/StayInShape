8321c5fc6363b519348897d75541fc1aa1bca73a
OWEE\trlfr <tr.lfrostbyte@gmail.com>
2024-11-13 11:40:10 +0800

    Add music into minigames

--- Assets/Scripts/BGMPlayer.cs
+++ Assets/Scripts/BGMPlayer.cs
@@ -4,24 +4,41 @@ using UnityEngine;
 
 public class BGMPlayer : MonoBehaviour
 {
-    static BGMPlayer instance;
+    //BGMPlayer instance;
     AudioSource audioSource;
 
     private void Awake()
     {
-        if (!instance)
-        {
-            instance = this;
-            DontDestroyOnLoad(this);
-            audioSource = this.GetComponent<AudioSource>();
-        }
-        else GameObject.Destroy(this);
+        //if (!instance)
+        //{
+        //    instance = this;
+        //    DontDestroyOnLoad(this);
+        //    audioSource = this.GetComponent<AudioSource>();
+        //}
+        //else GameObject.Destroy(this);
     }
 
     private void Start()
     {
         audioSource.Play();
         audioSource.loop = true;
+    }
+
+    public void StopMusic()
+    {
+        audioSource.Stop();
+    }
+
+    private void OnDestroy()
+    {
+        Debug.Log("On Destroy Pressed");
+        StopMusic();
+    }
+
+    void OnDisable()
+    {
+        Debug.Log("On Disable pressed!");
+        StopMusic();
     }
 
 }
